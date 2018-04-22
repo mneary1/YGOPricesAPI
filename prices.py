bd534fbebcd6154f9942f33d5e0dcbe86eccd630
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2018-04-22 20:39:53 -0300

    use f-strings

--- prices.py
+++ prices.py
@@ -4,7 +4,7 @@ import requests
 class YGOPricesAPI():
 
     def __init__(self):
-        self.url = "http://yugiohprices.com/api/"
+        self.url = "http://yugiohprices.com/api"
 
     def __make_request(self, url):
         """Request a resource from api"""
@@ -19,56 +19,55 @@ class YGOPricesAPI():
 
     def get_price_by_name(self, name):
         """Retrieves price data for every version of a card using its name"""
-        url = self.url + "get_card_prices/" + name
+        url = f"{self.url}/get_card_prices/{name}"
         return self.__make_request(url)
 
     def get_price_by_tag(self, tag, rarity=None):
         """Retrieve price data for a specific version of a card using its print tag"""
-        url = self.url
         if rarity:
-            url += "price_history/" + tag + "?rarity=" + rarity
+            url = f"{self.url}/price_history/{tag}?rarity={rarity}"
         else:
-            url += "price_for_print_tag/" + tag
+            url = f"{self.url}/price_for_print_tag/{tag}"
         return self.__make_request(url)
 
     def get_set_data(self, set_name):
         """Returns rarities and low/high/average prices for each card in the set."""
-        url = self.url + "set_data/" + set_name
+        url = f"{self.url}/set_data/{set_name}"
         return self.__make_request(url)
 
     def get_sets(self):
         """Retrieve list of all set names in Yugioh Prices database"""
-        url = self.url + "card_sets"
+        url = f"{self.url}/card_sets"
         return self.__make_request(url)
 
     def get_rising_and_falling(self):
         """Retrieve rising and falling cards list"""
-        url = self.url + "rising_and_falling"
+        url = f"{self.url}/rising_and_falling"
         return self.__make_request(url)
 
     def get_top_100(self, rarity=None):
         """Retrieve Top 100 most expensive cards"""
-        url = self.url + "top_100_cards"
+        url = f"{self.url}/top_100_cards"
         if rarity:
-            url += "?rarity=" + rarity
+            url = f"{url}?rarity={rarity}"
         return self.__make_request(url)
 
     def get_card_names(self):
         """Retrieve all cards name"""
-        url = self.url + "card_names"
+        url = f"{self.url}/card_names"
         return self.__make_request(url)
 
     def get_card_data(self, name):
         """Retrieve all information for a card using its name"""
-        url = self.url + "card_data/" + name
+        url = f"{self.url}/card_data/{name}"
         return self.__make_request(url)
 
     def get_card_versions(self, name):
         """Retrieve a list of all known versions of a card using its name"""
-        url = self.url + "card_versions/" + name
+        url = f"{self.url}/card_versions/{name}"
         return self.__make_request(url)
 
     def get_card_support(self, name):
         """Retrieve a list of support cards for a given card using its name"""
-        url = self.url + "card_support/" + name
+        url = f"{self.url}/card_support/{name}"
         return self.__make_request(url)

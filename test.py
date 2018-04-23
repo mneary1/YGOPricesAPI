63cd176e750bdcb51740fe12f39c212d234be285
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2018-04-22 21:30:37 -0300

    update test.py

--- test.py
+++ test.py
@@ -1,21 +1,23 @@
-from prices import YGOPriceAPI
-from json import dumps 
+from json import dumps
 
-api = YGOPriceAPI()
+import prices
+
+
+api = prices.YGOPricesAPI()
 
 done = False
 
 while not done:
     
-    choice = raw_input("name or tag: ")
+    choice = input("name or tag: ")
     prices = {}
 
     if "n" in choice:
-        name = raw_input("Enter a card name: ")
+        name = input("Enter a card name: ")
         prices = api.get_price_by_name(name)
     elif "t" in choice:
-        name = raw_input("Enter a tag: ")
+        name = input("Enter a tag: ")
         prices = api.get_price_by_name(name)
 
     if prices:
-        print dumps(prices, indent=2)
+        print(dumps(prices, indent=2))

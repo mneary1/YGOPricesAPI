aaec3e34383875596d22da512fca25665c3e90d7
Ronald Theodoro Nascimento Silva <RonaldTheodoro@users.noreply.github.com>
2018-05-23 16:32:08 -0300

    Update project to python 3.6 (#1)
    * update .gitignore
    
    * add requirements.txt and Pipfile
    
    * upgrade project to python 3
    
    * add docstrings
    
    * add Exception
    
    * use f-strings
    
    * Update README.md
    
    * update test.py

--- README.md
+++ README.md
@@ -1,17 +1,80 @@
 # YGOPricesAPI
-A Python wrapper around the yugiohprices.com API.
-I wanted to build an application using the yugiohprices.com API, but there weren't any wrappers for it.
 
-So I wrote one.
+A Python wrapper around the yugiohprices.com API.
 
 Usage:
 
 ```
 from prices import YGOPricesAPI
 api = YGOPricesAPI()
-# make api call
-# all api calls return the json response 
-# Ex:
-price = api.get_price_by_name("Blue-eyes White Dragon")
 ```
 
+## get_price_by_name
+
+### Retrieves price data for every version of a card using its name
+```
+>>> api.get_price_by_name('Blue-Eyes White Dragon')
+```
+
+## get_price_by_tag
+
+### Retrieve price data for a specific version of a card using its print tag
+```
+>>> api.get_price_by_tag('YSKR-EN001', rarity='Secret Rare')
+```
+
+## get_set_data
+
+### Returns rarities and low/high/average prices for each card in the set.
+```
+>>> api.get_set_data('Legend of Blue Eyes White Dragon')
+```
+
+## get_sets
+
+### Retrieve list of all set names in Yugioh Prices database
+```
+>>> api.get_sets()
+```
+        
+## get_rising_and_falling()
+
+### Retrieve rising and falling cards list
+```
+>>> api.get_rising_and_falling()
+```
+        
+## get_top_100(rarity=None)
+
+### Retrieve Top 100 most expensive cards
+```
+>>> api.get_top_100(rarity='Secret Rare')
+```
+        
+## get_card_names()
+
+### Retrieve all cards name
+```
+>>> api.get_card_names()
+```
+        
+## get_card_data(name)
+
+### Retrieve all information for a card using its name
+```
+>>> api.get_card_data('Blue-Eyes White Dragon')
+```
+        
+## get_card_versions(name)
+
+### Retrieve a list of all known versions of a card using its name
+```
+>>> api.get_card_versions('Blue-Eyes White Dragon')
+```
+        
+## get_card_support(name)
+
+### Retrieve a list of support cards for a given card using its name
+```
+>>> api.get_card_support('Blue-Eyes White Dragon')
+```
--- prices.py
+++ prices.py
@@ -1,64 +1,73 @@
 import requests
 
+
 class YGOPricesAPI():
+
     def __init__(self):
-        self.url = "http://yugiohprices.com/api/"
+        self.url = "http://yugiohprices.com/api"
 
     def __make_request(self, url):
+        """Request a resource from api"""
         request = requests.get(url)
-        
+
         if request.status_code != 200:
-            print request
-            print "Request status not OK. Returning None..."
-            return 
+            status_code = request.status_code
+            reason = request.reason
+            raise Exception(f'Status code: {status_code} Reason: {reason}')
 
         return request.json()
 
     def get_price_by_name(self, name):
-        url = self.url + "get_card_prices/" + name  
+        """Retrieves price data for every version of a card using its name"""
+        url = f"{self.url}/get_card_prices/{name}"
         return self.__make_request(url)
 
     def get_price_by_tag(self, tag, rarity=None):
-        url = self.url 
+        """Retrieve price data for a specific version of a card using its print tag"""
         if rarity:
-            url += "price_history/" + tag + "?rarity=" + rarity
-            print url
+            url = f"{self.url}/price_history/{tag}?rarity={rarity}"
         else:
-            url += "price_for_print_tag/" + tag
+            url = f"{self.url}/price_for_print_tag/{tag}"
         return self.__make_request(url)
 
     def get_set_data(self, set_name):
-        url = self.url + "set_data/" + set_name
+        """Returns rarities and low/high/average prices for each card in the set."""
+        url = f"{self.url}/set_data/{set_name}"
         return self.__make_request(url)
-    
+
     def get_sets(self):
-        url = self.url + "card_sets"
+        """Retrieve list of all set names in Yugioh Prices database"""
+        url = f"{self.url}/card_sets"
         return self.__make_request(url)
 
     def get_rising_and_falling(self):
-        url = self.url + "rising_and_falling"
+        """Retrieve rising and falling cards list"""
+        url = f"{self.url}/rising_and_falling"
         return self.__make_request(url)
 
     def get_top_100(self, rarity=None):
-        url = self.url + "top_100_cards"
+        """Retrieve Top 100 most expensive cards"""
+        url = f"{self.url}/top_100_cards"
         if rarity:
-            url += "?rarity=" + rarity
+            url = f"{url}?rarity={rarity}"
         return self.__make_request(url)
 
     def get_card_names(self):
-        url = self.url + "card_names"
+        """Retrieve all cards name"""
+        url = f"{self.url}/card_names"
         return self.__make_request(url)
 
     def get_card_data(self, name):
-        url = self.url + "card_data/" + name
+        """Retrieve all information for a card using its name"""
+        url = f"{self.url}/card_data/{name}"
         return self.__make_request(url)
 
     def get_card_versions(self, name):
-        url = self.url + "card_versions/" + name
+        """Retrieve a list of all known versions of a card using its name"""
+        url = f"{self.url}/card_versions/{name}"
         return self.__make_request(url)
 
     def get_card_support(self, name):
-        url = self.url + "card_support/" + name
+        """Retrieve a list of support cards for a given card using its name"""
+        url = f"{self.url}/card_support/{name}"
         return self.__make_request(url)
-
-
